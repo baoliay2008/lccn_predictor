5f81f2bb145cb4b232f750ea4db7b29d8db80ca3
Li Bao <baoliay2008@gmail.com>
2023-12-28 20:58:55 -0800

    🧪 test: add unit test for elo_delta function

---
+++ tests/test_elo.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+from app.core.elo import elo_delta
+
+
+def test_elo_delta():
+    """
+    Test the Elo rating system implementation.
+
+    Loads test data, calculates Elo deltas, and checks if the errors
+    between expected new ratings and calculated new ratings are within
+    a specified precision.
+
+    Raises:
+        AssertionError: If not all errors are within the specified precision.
+    """
+    PRECISION = 0.05
+
+    with open("tests/test_data/contest_k_rating_test.npy", "rb") as f:
+        data = np.load(f)
+        ks = data[:, 0]
+        ranks = data[:, 1]
+        old_ratings = data[:, 2]
+        new_ratings = data[:, 3]
+
+        delta_ratings = elo_delta(ranks, old_ratings, ks)
+        testing_new_ratings = old_ratings + delta_ratings
+
+        errors = np.abs(new_ratings - testing_new_ratings)
+        assert np.all(
+            errors < PRECISION
+        ), f"Elo delta test failed. Some errors are not within {PRECISION} precision."
