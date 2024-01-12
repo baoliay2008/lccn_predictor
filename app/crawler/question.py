201b52003f49181596d2dcb0e20caee29d7c4aac
Li Bao <baoliay2008@gmail.com>
2024-01-11 23:57:57 -0800

    ✨ feat: add a new function to retrieve questions data

--- app/crawler/question.py
+++ app/crawler/question.py
@@ -1,12 +1,42 @@
 import asyncio
 from datetime import datetime, timedelta
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from beanie.odm.operators.update.general import Set
 from loguru import logger
 
-from app.db.models import Question, Submission
+from app.crawler.utils import multi_http_request
+from app.db.models import DATA_REGION, Question, Submission
 from app.utils import get_contest_start_time
+
+
+async def request_questions(
+    contest_name: str,
+    data_region: DATA_REGION,
+) -> Optional[List[Dict]]:
+    """
+    Send HTTP request to get questions data of a given contest
+    :param contest_name:
+    :param data_region:
+    :return:
+    """
+    if data_region == "US":
+        url = f"https://leetcode.com/contest/api/info/{contest_name}/"
+    elif data_region == "CN":
+        url = f"https://leetcode.cn/contest/api/info/{contest_name}/"
+    else:
+        raise ValueError(f"{data_region=}")
+    data = (
+        await multi_http_request(
+            {
+                "req": {
+                    "url": url,
+                    "method": "GET",
+                }
+            }
+        )
+    )[0].json()
+    return data.get("questions")
 
 
 async def finish_count_at_time_point(
