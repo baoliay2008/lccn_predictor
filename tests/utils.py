bde9ac39a260a8ae148999e0ccd6e5970cb3deb9
Li Bao <baoliay2008@gmail.com>
2024-02-12 19:22:29 -0800

    🧪 test: Refactor tests directory structure, making it clearer

--- tests/utils.py
+++ tests/utils.py
@@ -7,7 +7,7 @@ RATING_DELTA_PRECISION: Final[float] = 0.05
 
 
 def read_data_contest_prediction_first():
-    with open("tests/test_data/contest_prediction_1.npy", "rb") as f:
+    with open("tests/tests_data/contest_prediction_1.npy", "rb") as f:
         data = np.load(f)
         ks = data[:, 0]
         ranks = data[:, 1]
