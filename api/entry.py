50228ebe8d361f8d518d1b07b85352118618c4c5
Li Bao <baoliay2008@gmail.com>
2024-03-13 20:27:28 -0700

    ✨ feat: Add middleware to handle API logging

--- api/entry.py
+++ api/entry.py
@@ -1,6 +1,9 @@
+import time
+
 from fastapi import APIRouter, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
+from loguru import logger
 
 from app.config import get_yaml_config
 from app.db.mongodb import start_async_mongodb
@@ -11,13 +14,6 @@ from .routers import contest_records, contests, questions
 app = FastAPI()
 yaml_config = get_yaml_config().get("fastapi")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=yaml_config.get("CORS_allow_origins"),
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(contests.router, prefix="/api/v1")
 app.include_router(contest_records.router, prefix="/api/v1")
@@ -28,6 +24,27 @@ app.include_router(questions.router, prefix="/api/v1")
 async def startup_event():
     start_loguru(process="api")
     await start_async_mongodb()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=yaml_config.get("CORS_allow_origins"),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    logger.info(
+        f"Received request: {request.client.host} {request.method} {request.url.path}"
+    )
+    t1 = time.time()
+    response = await call_next(request)
+    t2 = time.time()
+    logger.info(f"Cost {(t2 - t1) * 1e3:.2f} ms {response.status_code=}")
+    return response
 
 
 # Redirect Old API to new one
